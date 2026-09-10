88b621af3d257495ab04507de4b4f3e437a85c70
jch <ggongco1120@gmail.com>
2026-09-10 20:27:42 +0900

    해더 메뉴 1차 완료

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,25 +1,40 @@
 'use client';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser, faFile, faIdCard } from '@fortawesome/free-solid-svg-icons';
 
 const menuClickHandler = () => {
-    console.log("asdf");
+    const nav = document.querySelector('header > nav');
+    if (!nav) return;
+    
+    if (nav?.classList.contains('on')) {
+        nav.classList.remove('on');
+        document.body.style.overflow = 'auto'; // 스크롤 잠금 해제
+    }else{
+        nav?.classList.add('on');
+        document.body.style.overflow = 'hidden'; // 스크롤 잠금
+    }
 };
 
 const Header = () => {
     return(
-        <header className={`p-2 fixed top-3 left-3 bg-white shadow-md z-50 rounded-md cursor-pointer border border-indigo-100 hidden`}
-        onClick={menuClickHandler}
-        >
-            <p>
+        <header className={`fixed top-3 left-3 z-50 hidden`}>
+            <p 
+                onClick={menuClickHandler} 
+                className={`absolute top-0 left-0 z-40 p-2 bg-white shadow-md rounded-md cursor-pointer border border-indigo-100 w-fit`}
+            >
                 <FontAwesomeIcon icon={faBars} size="lg"/>
             </p>
-            <nav>
+
+            <nav className={`absolute top-0 left-0 z-30 p-2 bg-white shadow-md rounded-md border border-indigo-100`} >
                 <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
+                    <li><FontAwesomeIcon icon={faUser} size="lg"/></li>
+                    <li><FontAwesomeIcon icon={faFile} size="lg"/></li>
+                    <li><FontAwesomeIcon icon={faIdCard} size="lg"/></li>
                 </ul>
+                <div 
+                    className={`dk-wrap absolute -top-3 -left-3 w-screen h-screen bg-black opacity-50 hidden`}
+                    onClick={menuClickHandler} 
+                ></div>
             </nav>
         </header>
     );
