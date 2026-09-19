0ccb3e66ae53d9fa96e631c6ded2d7b565960f3c
jch <ggongco1120@gmail.com>
2026-09-19 13:14:01 +0900

    파비콘 변경 및 세팅값 변경

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["121.137.1.26"],
+  output: "export",
+  basePath: "/New_portfolios",
+  assetPrefix: "/New_portfolios",
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
