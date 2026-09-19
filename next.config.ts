411d513bd72d8310f2afd9b1a2be90097366e54f
jch <ggongco1120@gmail.com>
2026-09-19 13:40:55 +0900

    세팅 조정중

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ["121.137.1.26"],
   output: "export",
   basePath: "/New_portfolios",
-  assetPrefix: "/New_portfolios",  
   images: { unoptimized: true }
 };
 
